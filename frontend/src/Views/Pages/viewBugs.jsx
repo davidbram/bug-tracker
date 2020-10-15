c5e4202c5f92f267470979bb4ddc89baafe15df5
David Abraham <daveynoire97@gmail.com>
2020-10-16 02:42:37 +0530

    display bug details on click

--- frontend/src/Views/Pages/viewBugs.jsx
+++ frontend/src/Views/Pages/viewBugs.jsx
@@ -1,18 +1,26 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import {useDispatch,useSelector} from 'react-redux';
 import {getBugs} from '../../Controllers/Redux/bugSlice';
-import BugCard from '../Bug Card/bugCard';
+import BugCard from '../Components/Bug Card/bugCard';
+import BugView from '../Components/Bug view/bugView.jsx';
 
 export default()=>{
-
+    const [DISPLAY_BUG, SET_DISPLAY_BUG]=useState({
+        name:"",
+        isDisplayed:false
+    });
     const dispatch = useDispatch();
-    const {bugs} = useSelector(state=>state);
+    const { bugs } = useSelector(state=>state);
 
     useEffect(()=> {
         dispatch(getBugs());
     },[bugs.length < 1]);
 
     function BugClicked(name){
+        SET_DISPLAY_BUG({
+            isDisplayed: !DISPLAY_BUG.isDisplayed,
+            name:name,
+        });
 
     }
 
@@ -21,7 +29,7 @@ export default()=>{
         {bugs.map((bug,key)=>(
             <BugCard key={key} bug ={bug} clicked={BugClicked} />
             ))}
-
+        {DISPLAY_BUG.isDisplayed && <BugView clicked={BugClicked} bug={bugs.filter((bug) => bug.name == DISPLAY_BUG.name)[0]} />}
     </div> 
 
     )
