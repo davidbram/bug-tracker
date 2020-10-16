3002874f62078cbacbd16d3acf736f38f2db0e3f
vvk1999 <vinaykshirsagar1999@gmail.com>
2020-10-16 20:43:15 +0530

    Dashboard page debugged
    color property was not working

--- frontend/src/Views/Components/Dashboard/card.jsx
+++ frontend/src/Views/Components/Dashboard/card.jsx
@@ -3,9 +3,9 @@ import './card.css';
 import Priority from '../../../Controllers/priorityController'
 
 export default(props) =>{
-    const {level,color} = Priority(props.Priority);
+    const {level,color} = Priority(props.priority);
     return(
-        <div className='dashboard-card'>
+        <div className='dashboard-card' onClick={props.clicked} style={{color: color}}>
             <h2>Total: {level}</h2>
             <p>{props.count}</p>
         </div>
--- frontend/src/Views/Pages/dashboard.jsx
+++ frontend/src/Views/Pages/dashboard.jsx
@@ -2,10 +2,12 @@ import React,{useEffect} from 'react';
 import Card from '../Components/Dashboard/card';
 import {useDispatch,useSelector} from 'react-redux';
 import {getBugs} from '../../Controllers/Redux/bugSlice';
+import {useHistory} from 'react-router-dom';
 
 export default() =>{
     const dispatch = useDispatch();
     const bugs = useSelector(state=>state.bugs);
+    const browserHistory = useHistory();
     let midCount = 0;
     let highCount = 0;
     let lowCount = 0;
@@ -20,15 +22,19 @@ export default() =>{
         return bugs.filter((bug)=>{return bug.priority == priority});
     }
 
+    function redirect(){
+        browserHistory.push('/viewbugs');
+    }
+
     useEffect(()=>{
         dispatch(getBugs)
     },[bugs == undefined]
     )
     return(
         <div className="page-container">
-            <Card priority='1' count="10" />
-            <Card priority='2' count="10"/>
-            <Card priority='3' count="10"/>
+            <Card priority='1' count="10" clicked={redirect}/>
+            <Card priority='2' count="10" clicked={redirect}/>
+            <Card priority='3' count="10" clicked={redirect}/>
         </div>
         
     );
