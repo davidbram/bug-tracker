3ec6eecc7f11e84ec77f6c6acdc128cab2310273
David Abraham <daveynoire97@gmail.com>
2020-11-01 19:51:06 +0530

    added route handlers to handle server requests

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,10 @@
+require('dotenv').config();
+
 const express = require('express');
 
-const _ = require('lodash');
+// const _ = require('lodash');
 
-const path = require('path');
-
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 const mongoose = require('mongoose');
 
@@ -23,15 +23,135 @@ const bugSchema = new mongoose.Schema({
   details: String,
   steps: String,
   version: String,
-  priority: String,
+  priority: Number,
   assigned: String,
   creator: String,
   time: String,
+  status: String,
 });
 
 const Bug = mongoose.model('Bug', bugSchema);
 
-app.route('')
+// / crud operations on all bugs ///
+
+app.route('/bug')
+
+.get((req, res) => {
+  Bug.find({}, (err, foundBugs) => {
+    if (!err) {
+      if (foundBugs.length > 0) {
+        res.send(foundBugs);
+      } else {
+        res.send('No bugs');
+      }
+    } else {
+      res.send(err);
+    }
+  });
+})
+
+.post((req, res) => {
+  const bug = new Bug({
+    name: req.body.name,
+    details: req.body.details,
+    steps: req.body.steps,
+    version: req.body.version,
+    priority: req.body.priority,
+    assigned: req.body.assigned,
+    creator: req.body.creator,
+    time: req.body.time,
+    status: req.body.status,
+  });
+
+  bug.save((err) => {
+    if (!err) {
+      res.send('Successfully saved bug in DB');
+    } else {
+      res.send(err);
+    }
+  });
+})
+
+.delete((req, res) => {
+  Bug.deleteMany({}, (err) => {
+    if (!err) {
+      res.send('Deleted all bugs');
+    } else {
+      res.send(err);
+    }
+  },
+  );
+});
+// / crud operations on specific bug ///
+
+app.route('/bug/:bugName')
+
+.get((req, res) => {
+  Bug.findOne({ name: req.params.bugName }, (err, foundBug) => {
+    if (!err) {
+      if (foundBug) {
+        res.send(foundBug);
+      } else {
+        res.send('Bug not found');
+      }
+    } else {
+      res.send(err);
+    }
+  });
+})
+
+.put((req, res) => {
+  Bug.update(
+    { name: req.params.bugName },
+    {
+      name: req.body.name,
+      details: req.body.details,
+      steps: req.body.steps,
+      version: req.body.version,
+      priority: req.body.priority,
+      assigned: req.body.assigned,
+      creator: req.body.creator,
+      time: req.body.time,
+      status: req.body.status,
+    },
+    { overwrite: true },
+    (err) => {
+      if (!err) {
+        res.send(`${req.params.bugName} bug updated successfully `);
+      } else {
+        res.send(err);
+      }
+    },
+
+  );
+})
+
+.patch((req, res) => {
+  Bug.update(
+    { name: req.params.bugName },
+    { $set: req.body },
+    (err) => {
+      if (!err) {
+        res.send(`${req.params.bugName} bug updated successfully `);
+      } else {
+        res.send(err);
+      }
+    },
+  );
+})
+
+.delete((req, res) => {
+  Bug.deleteOne(
+  { name: req.params.bugName },
+  (err) => {
+    if (!err) {
+      res.send(`${req.params.bugName} bug has been deleted successfully`);
+    } else {
+      res.send(err);
+    }
+  },
+);
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on ${PORT}`);
