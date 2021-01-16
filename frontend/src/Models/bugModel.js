b875737990ee0167b7b2faf018b8d0b052d04f34
vvk1999 <vinaykshirsagar1999@gmail.com>
2021-01-16 12:11:22 +0530

    Added Seperate tabs for completed and open bugs

--- frontend/src/Models/bugModel.js
+++ frontend/src/Models/bugModel.js
@@ -12,5 +12,6 @@ function bug(bug){
         this.assigned = bug.assigned;
         this.creator = bug.creator;
         this.time = bug.time;
+        this.status = bug.status
     }
 }
--- frontend/src/Views/Pages/viewBugs.jsx
+++ frontend/src/Views/Pages/viewBugs.jsx
@@ -6,9 +6,61 @@ import BugView from '../Components/Bug view/bugView.jsx';
 import bugModel from '../../Models/bugModel';
 import axios from "axios";
 
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
 
+function TabPanel(props) {
+    const { children, value, index, ...other } = props;
+  
+    return (
+      <div
+        role="tabpanel"
+        hidden={value !== index}
+        id={`simple-tabpanel-${index}`}
+        aria-labelledby={`simple-tab-${index}`}
+        {...other}
+      >
+        {value === index && (
+          <Box p={3}>
+            <Typography>{children}</Typography>
+          </Box>
+        )}
+      </div>
+    );
+  }
 
-export default()=>{
+export default(props)=>{
+
+    const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+    return(
+        <div className="page-container" style={{position:'static'}}>
+        <AppBar position="static" style={{backgroundColor: '#264e70', width:330,height:50, position:'absolute'}}>
+  <Tabs value={value} onChange={handleChange} aria-label="simple tabs example" >
+    <Tab label="Open Bugs" style={{position:'static'}} />
+    <Tab label="Completed Bugs" style={{position:'static'}} />
+   
+  </Tabs>
+</AppBar>
+<TabPanel value={value} index={0} position="static" style={{ paddingTop :50}}>
+  <SortedBugs mode="open"  />
+</TabPanel>
+<TabPanel value={value} index={1} position="static" style={{ paddingTop :50}}>
+<SortedBugs mode="completed" />
+</TabPanel>
+
+</div>
+    )
+}
+
+function SortedBugs(props){
     const [bugs, setBugs] = useState([]);
 
     const [DISPLAY_BUG, SET_DISPLAY_BUG]=useState({
@@ -21,8 +73,9 @@ export default()=>{
     useEffect(() => {
        axios.get("/api/bug")
        .then((response) => {
-            const data = response.data.map(bug => new bugModel(bug));
-            const sorted = data.sort((a,b)=>{return a.priority-b.priority;});
+            var data = response.data.map(bug => new bugModel(bug));
+            data = data.filter(bug => bug.status==props.mode)
+            const sorted = data.sort((a,b)=>{return a.priority-b.priority});
             setBugs(sorted);
           });
     }, []);
@@ -51,6 +104,7 @@ export default()=>{
 
     return(
     <div className="page-container">
+        
         {bugs.map((bug,key)=>(
             <BugCard key ={key} bug={bug} clicked={BugClicked} />
             ))}
